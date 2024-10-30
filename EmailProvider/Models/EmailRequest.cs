ec54272442ae4706e4e1a113682110706b4fcef9
Nour <deen80@live.com>
2024-10-30 13:02:09 +0100

    Models Added

---
+++ EmailProvider/Models/EmailRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace EmailProvider.Models;
+
+// Represents an email request with necessary details to send an email
+public class EmailRequest
+{
+    // Recipient's email address  - Hassan
+    public string To { get; set; } = null!;
+
+    // Subject of the email
+    public string Subject { get; set; } = null!;
+
+    // HTML content of the email body
+    public string HtmlBody { get; set; } = null!;
+
+    // Plain text version of the email body for clients that do not support HTML
+    public string PlainText { get; set; } = null!;
+}
+
+
+
+
